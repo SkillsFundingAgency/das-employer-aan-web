4021765030fdd553f92c78df46bbd691ab0a7556
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-05-10 17:10:50 +0100

    CSP-525-Implemented TnC controller, view, TC

---
+++ src/SFA.DAS.Employer.Aan.Web.UnitTests/Controllers/Onboarding/TermsAndConditionsControllerAttributeTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Employer.Aan.Web.Controllers.Onboarding;
+
+namespace SFA.DAS.Employer.Aan.Web.UnitTests.Controllers.Onboarding;
+
+
+[TestFixture]
+public class TermsAndConditionsControllerAttributeTests
+{
+    [Test]
+    public void Controller_HasCorrectAttributes()
+    {
+        typeof(TermsAndConditionsController).Should().BeDecoratedWith<RouteAttribute>();
+        typeof(TermsAndConditionsController).Should().BeDecoratedWith<RouteAttribute>().Subject.Template.Should().Be("onboarding/terms-and-conditions");
+        typeof(TermsAndConditionsController).Should().BeDecoratedWith<RouteAttribute>().Subject.Name.Should().Be("TermsAndConditions");
+    }
+}
--- src/SFA.DAS.Employer.Aan.Web/Controllers/Onboarding/BeforeYouStartController.cs
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/Onboarding/BeforeYouStartController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Employer.Aan.Web.Infrastructure;
 
 namespace SFA.DAS.Employer.Aan.Web.Controllers.Onboarding;
 
-[Route("onboarding/before-you-start")]
+[Route("onboarding/before-you-start", Name = RouteNames.Onboarding.BeforeYouStart)]
 public class BeforeYouStartController : Controller
 {
     public const string ViewPath = "~/Views/Onboarding/BeforeYouStart.cshtml";
