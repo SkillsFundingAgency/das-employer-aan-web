8dd9564446d26c9b04837367ed2e09ce4674452c
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-05-31 23:34:01 +0100

    CSP-547 Added Controller,View, Validator

---
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/Onboarding/PreviousEngagementController.cs
@@ -0,0 +1,65 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Employer.Aan.Domain.Constants;
+using SFA.DAS.Employer.Aan.Domain.Interfaces;
+using SFA.DAS.Employer.Aan.Web.Infrastructure;
+using SFA.DAS.Employer.Aan.Web.Models.Onboarding;
+
+namespace SFA.DAS.Employer.Aan.Web.Controllers.Onboarding;
+
+[Route("onboarding/previous-engagement", Name = RouteNames.Onboarding.PreviousEngagement)]
+public class PreviousEngagementController : Controller
+{
+    public const string ViewPath = "~/Views/Onboarding/PreviousEngagement.cshtml";
+    private readonly ISessionService _sessionService;
+    private readonly IValidator<PreviousEngagementSubmitModel> _validator;
+
+    public PreviousEngagementController(ISessionService sessionService,
+        IValidator<PreviousEngagementSubmitModel> validator)
+    {
+        _validator = validator;
+        _sessionService = sessionService;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        var sessionModel = _sessionService.Get<OnboardingSessionModel>();
+
+        var model = GetViewModel(sessionModel);
+        return View(ViewPath, model);
+    }
+
+    [HttpPost]
+    public IActionResult Post(PreviousEngagementSubmitModel submitmodel)
+    {
+        var sessionModel = _sessionService.Get<OnboardingSessionModel>();
+        var model = GetViewModel(sessionModel);
+
+        ValidationResult result = _validator.Validate(submitmodel);
+
+        if (!result.IsValid)
+        {
+            result.AddToModelState(this.ModelState);
+            return View(ViewPath, model);
+        }
+
+        sessionModel.SetProfileValue(ProfileDataId.HasPreviousEngagement, submitmodel.HasPreviousEngagement.ToString()!);
+
+        _sessionService.Set(sessionModel);
+
+        return View(ViewPath, model);
+    }
+
+    private PreviousEngagementViewModel GetViewModel(OnboardingSessionModel sessionModel)
+    {
+        var previousEngagement = sessionModel.GetProfileValue(ProfileDataId.HasPreviousEngagement);
+        return new PreviousEngagementViewModel()
+        {
+            HasPreviousEngagement = bool.TryParse(previousEngagement, out var result) ? result : null,
+            BackLink = Url.RouteUrl(@RouteNames.Onboarding.JoinTheNetwork)!
+        };
+    }
+}
---
+++ src/SFA.DAS.Employer.Aan.Web/Validators/PreviousEngagementSubmitModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using SFA.DAS.Employer.Aan.Web.Models.Onboarding;
+
+namespace SFA.DAS.Employer.Aan.Web.Validators;
+
+public class PreviousEngagementSubmitModelValidator : AbstractValidator<PreviousEngagementSubmitModel>
+{
+    public const string NoSelectionErrorMessage = "Tell us if you have engaged with an ambassador in the network";
+
+    public PreviousEngagementSubmitModelValidator()
+    {
+        RuleFor(m => m.HasPreviousEngagement)
+            .NotNull()
+            .WithMessage(NoSelectionErrorMessage);
+    }
+}
