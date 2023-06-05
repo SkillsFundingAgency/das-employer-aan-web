b0d297534a8c46395a1d4c70fc23b4b6f53d363d
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-06-06 00:09:18 +0100

    CSP-578 Implemented View, Controller, TC

---
+++ src/SFA.DAS.Employer.Aan.Web.UnitTests/Controllers/Onboarding/AreasToEngageLocallyControllerTests/AreasToEngageLocallyControllerAttributeTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Employer.Aan.Web.Controllers.Onboarding;
+
+namespace SFA.DAS.Employer.Aan.Web.UnitTests.Controllers.Onboarding.AreasToEngageLocallyControllerTests;
+
+[TestFixture]
+public class AreasToEngageLocallyControllerAttributeTests
+{
+    [Test]
+    public void Controller_HasCorrectAttributes()
+    {
+        typeof(AreasToEngageLocallyController).Should().BeDecoratedWith<RouteAttribute>();
+        typeof(AreasToEngageLocallyController).Should().BeDecoratedWith<RouteAttribute>().Subject.Template.Should().Be("onboarding/areas-to-engage-locally");
+        typeof(AreasToEngageLocallyController).Should().BeDecoratedWith<RouteAttribute>().Subject.Name.Should().Be("AreasToEngageLocally");
+    }
+}
