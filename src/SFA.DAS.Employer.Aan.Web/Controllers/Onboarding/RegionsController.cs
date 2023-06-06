bbbe01b2507cb7a03d90cae3e4716a47e68f7722
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-06-06 16:29:19 +0100

    CSP-578 Resolving merge issue

--- src/SFA.DAS.Employer.Aan.Web/Controllers/Onboarding/RegionsController.cs
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/Onboarding/RegionsController.cs
@@ -34,11 +34,12 @@ public class RegionsController : Controller
     [HttpPost]
     public async Task<IActionResult> Post(RegionsSubmitModel submitModel, CancellationToken cancellationToken)
     {
+        var model = await GetViewModel(cancellationToken);
         ValidationResult result = _validator.Validate(submitModel);
 
         if (!result.IsValid)
         {
-            var model = await GetViewModel(cancellationToken);
+
             result.AddToModelState(ModelState);
             return View(ViewPath, model);
         }
@@ -52,13 +53,9 @@ public class RegionsController : Controller
         {
             return RedirectToRoute(RouteNames.Onboarding.JoinTheNetwork);
         }
-        else if (sessionModel.Regions.Count(x => x.IsSelected) >= 2 && sessionModel.Regions.Count(x => x.IsSelected) <= 4)
-        {
-            return RedirectToRoute(RouteNames.Onboarding.AreasToEngageLocally);
-        }
         else
         {
-            return RedirectToRoute(RouteNames.Onboarding.Regions);
+            return View(ViewPath, model);
         }
     }
 
