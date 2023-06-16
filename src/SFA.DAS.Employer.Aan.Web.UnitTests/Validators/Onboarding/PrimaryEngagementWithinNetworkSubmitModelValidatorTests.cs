666e99acd3ba82bb5523d0599f65bb2705ed8620
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-06-16 14:32:59 +0100

    CSP-577 Minor namespace formatting

--- src/SFA.DAS.Employer.Aan.Web.UnitTests/Validators/Onboarding/PrimaryEngagementWithinNetworkSubmitModelValidatorTests.cs
+++ src/SFA.DAS.Employer.Aan.Web.UnitTests/Validators/Onboarding/PrimaryEngagementWithinNetworkSubmitModelValidatorTests.cs
@@ -2,25 +2,24 @@
 using SFA.DAS.Employer.Aan.Web.Models.Onboarding;
 using SFA.DAS.Employer.Aan.Web.Validators;
 
-namespace SFA.DAS.Employer.Aan.Web.UnitTests.Validators.Onboarding
+namespace SFA.DAS.Employer.Aan.Web.UnitTests.Validators.Onboarding;
+
+[TestFixture]
+public class PrimaryEngagementWithinNetworkSubmitModelValidatorTests
 {
-    [TestFixture]
-    public class PrimaryEngagementWithinNetworkSubmitModelValidatorTests
+    [TestCase(true, true)]
+    [TestCase(false, true)]
+    [TestCase(null, false)]
+    public void EngagedWithAnAmbassadorInTheNetwork_Validation(bool? value, bool isValid)
     {
-        [TestCase(true, true)]
-        [TestCase(false, true)]
-        [TestCase(null, false)]
-        public void EngagedWithAnAmbassadorInTheNetwork_Validation(bool? value, bool isValid)
-        {
-            var model = new PrimaryEngagementWithinNetworkSubmitModel { IsLocalOrganisation = value };
-            var sut = new PrimaryEngagementWithinNetworkSubmitModelValidator();
+        var model = new PrimaryEngagementWithinNetworkSubmitModel { IsLocalOrganisation = value };
+        var sut = new PrimaryEngagementWithinNetworkSubmitModelValidator();
 
-            var result = sut.TestValidate(model);
+        var result = sut.TestValidate(model);
 
-            if (isValid)
-                result.ShouldNotHaveValidationErrorFor(c => c.IsLocalOrganisation);
-            else
-                result.ShouldHaveValidationErrorFor(x => x.IsLocalOrganisation).WithErrorMessage(PrimaryEngagementWithinNetworkSubmitModelValidator.NoSelectionErrorMessage);
-        }
+        if (isValid)
+            result.ShouldNotHaveValidationErrorFor(c => c.IsLocalOrganisation);
+        else
+            result.ShouldHaveValidationErrorFor(x => x.IsLocalOrganisation).WithErrorMessage(PrimaryEngagementWithinNetworkSubmitModelValidator.NoSelectionErrorMessage);
     }
 }
