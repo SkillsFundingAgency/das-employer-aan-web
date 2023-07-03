656af9bae4192b3002481d717086303c1eafed0a
Deven Shah <devenshah1974@gmail.com>
2023-07-03 18:05:00 +0100

    CSP-717 Added empty EventsHub controller and view

---
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/EventsHubController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Employer.Aan.Web.Authentication;
+using SFA.DAS.Employer.Aan.Web.Infrastructure;
+
+namespace SFA.DAS.Employer.Aan.Web.Controllers;
+
+[Authorize(Policy = nameof(PolicyNames.HasEmployerAccount))]
+[Route("accounts/{employerAccountId}/events-hub", Name = RouteNames.EventsHub)]
+public class EventsHubController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
