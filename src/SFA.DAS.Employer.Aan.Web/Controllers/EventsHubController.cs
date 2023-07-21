6335d4406d0c8293641b1d2b6df653dd268ed771
Deven Shah <devenshah1974@gmail.com>
2023-07-21 16:50:11 +0100

    CSP-882 Added reference to SharedUi

---
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/EventsHubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Aan.SharedUi.Models;
+
+namespace SFA.DAS.Employer.Aan.Web.Controllers;
+public class EventsHubController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new EventsHubViewModel(new DateOnly(DateTime.Today.Year, DateTime.Today.Month, 1), Url, new()));
+    }
+}
