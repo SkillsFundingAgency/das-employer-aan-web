ac3423bccb72bfce627b4ccf4dcd78cca9a7be9f
Deven Shah <devenshah1974@gmail.com>
2023-08-21 08:55:30 +0100

    CSP-979 Added some notes

--- src/SFA.DAS.Employer.Aan.Web/Controllers/LinksController.cs
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/LinksController.cs
@@ -14,12 +14,40 @@ public class LinksController : Controller
 
         string employerAccountId = GetEncodedAccountId(notification.EmployerAccountId);
 
+        var routeName = notification.TemplateName switch
+        {
+            "Onboarding" => RouteNames.NetworkHub,
+            "Sign-Up" => RedirectToRoute(RouteNames.EventDetails, new { }
+        };
+
         return RedirectToRoute(RouteNames.PublicProfile, new { employerAccountId = employerAccountId, memberId = notification.MemberId });
     }
 
-    private Notification GetNotification(Guid notificationId) => new(Guid.NewGuid(), 15601);
+    private Notification GetNotification(Guid notificationId) => new(Guid.NewGuid(), 15601, "Onboarding", "{'parametervalue'=''}");
 
     private string GetEncodedAccountId(long accountId) => "v6gr7d";
 }
 
-public record Notification(Guid MemberId, long EmployerAccountId);
+public record Notification(Guid MemberId, long EmployerAccountId, string TemplateName, string Tokens);
+
+
+record Token(string ParameterValue, string Link);
+
+
+/*
+ * rehydration of link logic is in web
+ * Job will inject the host in the token (EmpAanBaseUrl, AppAanBaseUrl)
+ * 
+ * 
+ * */
+
+/* Tokens AccountId, EventId, ParameterValue, NotificationId, BaseUrl
+
+
+/// {{link}}
+
+/// {hostname}/links/{notificationId}
+
+
+
+
