d5405513852983d052efbd4c1954a0c74bd006d2
Deven Shah <devenshah1974@gmail.com>
2023-10-17 18:11:51 +0100

    CSP-975 Added notification template name constants

---
+++ src/SFA.DAS.Employer.Aan.Domain/Constants/NotificationTemplateNames.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.Employer.Aan.Domain.Constants;
+
+public static class NotificationTemplateNames
+{
+    public const string AANEmployerOnboarding = nameof(AANEmployerOnboarding);
+    public const string AANEmployerEventSignup = nameof(AANEmployerEventSignup);
+    public const string AANEmployerEventCancel = nameof(AANEmployerEventCancel);
+    public const string AANEmployerWithdrawal = nameof(AANEmployerWithdrawal);
+    public const string AANAdminEventUpdate = nameof(AANAdminEventUpdate);
+    public const string AANAdminEventCancel = nameof(AANAdminEventCancel);
+    public const string AANIndustryAdvice = nameof(AANIndustryAdvice);
+    public const string AANAskForHelp = nameof(AANAskForHelp);
+    public const string AANRequestCaseStudy = nameof(AANRequestCaseStudy);
+    public const string AANGetInTouch = nameof(AANGetInTouch);
+}
