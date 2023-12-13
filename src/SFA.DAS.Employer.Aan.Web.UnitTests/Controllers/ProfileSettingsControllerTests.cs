f4e34033d5cb81bd6c6aa988cacc994cba1ce86a
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-12-13 13:19:11 +0000

    CSP-855 Show NetworkHubLink in Profile Settings page

--- src/SFA.DAS.Employer.Aan.Web.UnitTests/Controllers/ProfileSettingsControllerTests.cs
+++ src/SFA.DAS.Employer.Aan.Web.UnitTests/Controllers/ProfileSettingsControllerTests.cs
@@ -2,18 +2,22 @@
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Aan.SharedUi.Models;
 using SFA.DAS.Employer.Aan.Web.Controllers;
+using SFA.DAS.Employer.Aan.Web.Infrastructure;
+using SFA.DAS.Employer.Aan.Web.UnitTests.TestHelpers;
 
 namespace SFA.DAS.Employer.Aan.Web.UnitTests.Controllers;
 public class ProfileSettingsControllerTests
 {
     private IActionResult _result = null!;
+    private string employerAccountId = Guid.NewGuid().ToString();
+    private string NetworkHubUrl = Guid.NewGuid().ToString();
 
     [SetUp]
     public void WhenGettingNetworkHub()
     {
         ProfileSettingsController sut = new();
-
-        _result = sut.Index();
+        sut.AddUrlHelperMock().AddUrlForRoute(RouteNames.NetworkHub, NetworkHubUrl);
+        _result = sut.Index(employerAccountId);
     }
 
     [Test]
@@ -34,5 +38,12 @@ public class ProfileSettingsControllerTests
     {
         var model = _result.As<ViewResult>().Model.As<ProfileSettingsViewModel>();
         model.LeaveTheNetworkUrl.Should().Be("#");
+    }
+
+    [Test]
+    public void ThenSetsNetworkHubLinkUrlInViewModel()
+    {
+        var model = _result.As<ViewResult>().Model.As<ProfileSettingsViewModel>();
+        model.NetworkHubLink.Should().Be(NetworkHubUrl);
     }
 }
--- src/SFA.DAS.Employer.Aan.Web/Controllers/ProfileSettingsController.cs
+++ src/SFA.DAS.Employer.Aan.Web/Controllers/ProfileSettingsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Aan.SharedUi.Infrastructure;
 using SFA.DAS.Aan.SharedUi.Models;
 using SFA.DAS.Employer.Aan.Web.Authentication;
+using SFA.DAS.Employer.Aan.Web.Infrastructure;
 
 namespace SFA.DAS.Employer.Aan.Web.Controllers;
 
@@ -10,9 +11,12 @@ namespace SFA.DAS.Employer.Aan.Web.Controllers;
 [Route("accounts/{employerAccountId}/profile-settings", Name = SharedRouteNames.ProfileSettings)]
 public class ProfileSettingsController : Controller
 {
-    public IActionResult Index()
+    public IActionResult Index([FromRoute] string employerAccountId)
     {
-        var model = new ProfileSettingsViewModel();
+        var model = new ProfileSettingsViewModel()
+        {
+            NetworkHubLink = Url.RouteUrl(RouteNames.NetworkHub, new { employerAccountId = employerAccountId })
+        };
         return View(model);
     }
 }
