8ae7517ccc75a2e790701fbf287b5ce48302838e
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2024-01-11 14:42:01 +0000

    CSP-1200 Added length restriction for LinkedIn url

--- src/SFA.DAS.Employer.Aan.Web/Validators/EditContactDetail/SubmitContactDetailModelValidator.cs
+++ src/SFA.DAS.Employer.Aan.Web/Validators/EditContactDetail/SubmitContactDetailModelValidator.cs
@@ -6,9 +6,14 @@ namespace SFA.DAS.Employer.Aan.Web.Validators.EditContactDetail;
 public class SubmitContactDetailModelValidator : AbstractValidator<SubmitContactDetailModel>
 {
     public const string ShowLinkedinUrlValidationMessage = "You cannot select display check box without a value";
+    public const string AlphaNumericCharactersRegex = "^[a-zA-Z0-9]*$";
+    public const string LinkedinUrlLengthValidationMessage = "Your custom URL must contain 3-100 letters or numbers";
+    public const string LinkedinUrlPatternValidationMessage = "Your custom URL must not use spaces, symbols or special characters";
     public SubmitContactDetailModelValidator()
     {
         RuleLevelCascadeMode = CascadeMode.Stop;
+        RuleFor(x => x.LinkedinUrl).NotNull().WithMessage(LinkedinUrlLengthValidationMessage).Length(3, 100).WithMessage(LinkedinUrlLengthValidationMessage).Matches(AlphaNumericCharactersRegex)
+    .WithMessage(LinkedinUrlPatternValidationMessage);
         RuleFor(x => x.ShowLinkedinUrl)
             .Must((contactdetail, showlinkedinurl) => !showlinkedinurl)
             .When(x => string.IsNullOrEmpty(x.LinkedinUrl))
