97db844bdcfb773d4fc9bfa9be28db7c0a6739cf
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2024-01-11 16:00:07 +0000

    CSP-1200 Allowed dash in linked in link

--- src/SFA.DAS.Employer.Aan.Web/Validators/EditContactDetail/SubmitContactDetailModelValidator.cs
+++ src/SFA.DAS.Employer.Aan.Web/Validators/EditContactDetail/SubmitContactDetailModelValidator.cs
@@ -6,7 +6,7 @@ namespace SFA.DAS.Employer.Aan.Web.Validators.EditContactDetail;
 public class SubmitContactDetailModelValidator : AbstractValidator<SubmitContactDetailModel>
 {
     public const string ShowLinkedinUrlValidationMessage = "You cannot select display check box without a value";
-    public const string AlphaNumericCharactersRegex = "^[a-zA-Z0-9]*$";
+    public const string AlphaNumericCharactersRegex = "^[a-zA-Z0-9-]*$";
     public const string LinkedinUrlLengthValidationMessage = "Your custom URL must contain 3-100 letters or numbers";
     public const string LinkedinUrlPatternValidationMessage = "Your custom URL must not use spaces, symbols or special characters";
     public SubmitContactDetailModelValidator()
