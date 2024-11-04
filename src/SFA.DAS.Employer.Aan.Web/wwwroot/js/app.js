ee2d85971f0403a4bdc5100e8bff678953ce2c72
David Rimar <david.rimar@digital.education.gov.uk>
2024-11-04 15:12:20 +0000

    EC-762 add nodeListForEach function

--- src/SFA.DAS.Employer.Aan.Web/wwwroot/js/app.js
+++ src/SFA.DAS.Employer.Aan.Web/wwwroot/js/app.js
@@ -1,4 +1,13 @@
-﻿// Select Submit
+﻿function nodeListForEach(nodes, callback) {
+    if (window.NodeList.prototype.forEach) {
+        return nodes.forEach(callback);
+    }
+    for (let i = 0; i < nodes.length; i++) {
+        callback.call(window, nodes[i], i, nodes);
+    }
+}
+
+// Select Submit
 function SelectSubmit(selectField) {
     this.selectField = selectField;
     this.form = selectField.closest("form");
