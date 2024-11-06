9c47f6d53396595356fb88c7cacc83801251d82d
David Rimar <david.rimar@digital.education.gov.uk>
2024-11-06 10:55:58 +0000

    EC-762 remove unused js

--- src/SFA.DAS.Employer.Aan.Web/wwwroot/js/app.js
+++ src/SFA.DAS.Employer.Aan.Web/wwwroot/js/app.js
@@ -6,36 +6,6 @@ AutoComplete.prototype.init = function () {
     this.autoComplete()
 }
 
-AutoComplete.prototype.getSuggestions = function (query, updateResults) {
-    let results = [];
-    let apiUrl = "/locations?query=" + query
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            let jsonResponse = JSON.parse(xhr.responseText);
-            results = jsonResponse.map(function (result) {
-                return result
-            });
-            updateResults(results);
-        }
-    }
-    xhr.open("GET", apiUrl, true);
-    xhr.send();
-}
-
-AutoComplete.prototype.onConfirm = function (option) {
-    // Populate form fields with selected option
-    document.getElementById("OrganisationName").value = option.organisationName
-    document.getElementById("AddressLine1").value = option.addressLine1
-    document.getElementById("AddressLine2").value = option.addressLine2
-    document.getElementById("Town").value = option.town
-    document.getElementById("County").value = option.county
-    document.getElementById("Postcode").value = option.postcode
-    document.getElementById("Longitude").value = option.longitude
-    document.getElementById("Latitude").value = option.latitude
-
-}
-
 function inputValueTemplate(result) {
     return result && [result.organisationName, result.addressLine1, result.town, result.postcode].filter(element => element).join(', ')
 }
